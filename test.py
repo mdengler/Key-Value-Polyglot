98f0aa153e072358f6b4b9f8a51b62582908c0d7
Martin Dengler <martin@martindengler.com>
2012-03-21 18:18:57 +0800

    test.py: change to be compatible with Fedora python2

--- test.py
+++ test.py
@@ -1,22 +1,21 @@
-import pylibmc
+import memcache
 
-mc = pylibmc.Client(["127.0.0.1"])
+mc = memcache.Client(["127.0.0.1:11211"])
 
 # Check empty behavior
-try:
-    mc[str(-1)]
-    assert(False)
-except KeyError:
-    pass
+result = mc.get(str(-1))
+assert(result is None), "instead of exception got %s" % result
 
 # Check \r\n in data
-mc['br'] = 'one\r\ntwo'
-assert(mc['br'] == 'one\r\ntwo')
+desired = 'one\r\ntwo'
+mc.set('br', desired)
+result = mc.get('br')
+assert(result == 'one\r\ntwo'), "instead of [%s] got [%s]" % (desired, result)
 
 # set
 for i in range(500):
-    mc[str(i)] = i          # This hashmap syntax writes to memcached
+    mc.set(str(i), i)          # This hashmap syntax writes to memcached
 # get
 for i in range(500):
-    val = mc[str(i)]        # Hashmap syntax reads from memcached
+    val = mc.get(str(i))        # Hashmap syntax reads from memcached
     assert(int(val) == i)
